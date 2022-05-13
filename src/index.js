38474ba189f31786c5b36eff5ebddeb09cb067b3
pl3lee <billylee123@gmail.com>
2022-05-13 12:45:57 -0400

    eslint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import DisplayController from './displayController.js'
+import DisplayController from './displayController.js';
 import Project from './project.js';
 import ProjectList from './projectList.js';
 import Todo from './todo.js';
@@ -7,14 +7,14 @@ import Todo from './todo.js';
 const WebpageController = (() => {
     const setUpWebpage = () => {
         DisplayController.setupDefaultEventListeners();
-        let defaultProject = new Project("Default", 0);
+        const defaultProject = new Project('Default', 0);
         addProject(defaultProject);
-    }
+    };
     const addProject = (project) => {
         ProjectList.addProjectExisting(project);
         DisplayController.addProject(project);
     };
-    
+
     return {
         setUpWebpage,
         addProject,
@@ -22,4 +22,3 @@ const WebpageController = (() => {
 })();
 
 WebpageController.setUpWebpage();
-
